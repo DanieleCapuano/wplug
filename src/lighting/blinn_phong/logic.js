8954a5e0cdce2aec825852203e69443d13f2419d
DanieleCapuano <el.einad@gmail.com>
2023-02-24 15:37:21 +0100

    blinn_phong logic

--- src/lighting/blinn_phong/logic.js
+++ src/lighting/blinn_phong/logic.js
@@ -1,6 +1,7 @@
-export const get_lighting_conf = _get_lighting_conf;
+export const get_description_values = _get_desc;
+export const set_uniforms_values = _set_uniforms_values;
 
-function _get_lighting_conf(scene_desc) {
+function _get_desc(scene_desc) {
     const { lighting } = scene_desc;
     if (!lighting) return {};
 
@@ -11,4 +12,20 @@ function _get_lighting_conf(scene_desc) {
     lighting.number_of_lights = lighting.lights.length;
 
     return { lighting };
+}
+
+function _set_uniforms_values(obj, scene_desc) {
+    const { lighting } = scene_desc;
+    return {
+        u_ka: obj.material.ka,
+        u_kd: obj.material.kd,
+        u_ks: obj.material.ks,
+        u_nlights: lighting.number_of_lights,
+        u_ambient_color: lighting.ambient.color,
+        u_ambient_intensity: lighting.ambient.intensity,
+        u_light_positions: lighting.light_positions,
+        u_light_colors: lighting.light_colors,
+        u_light_intensities: lighting.light_intensities,
+        u_light_specular_exp: lighting.light_specular_exp
+    };
 }
