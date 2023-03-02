ee8ff733b02eb32e4c490988e33bd3483877baf0
DanieleCapuano <el.einad@gmail.com>
2023-03-02 07:59:27 +0100

    flatten logic fixed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import * as LMod from "./lighting";
 
 const L = LMod.default;
-const lighting = Object.keys(L).reduce((o, l_key) => Object.assign(o, {
-    [l_key]: L[l_key].default
-}), {});
+const lighting = _flatten_logic_into(L);
+
+function _flatten_logic_into(main_o) {
+    return Object.keys(main_o).reduce((o, l_key) => {
+        let default_o = main_o[l_key].default,
+            logic = default_o.logic || {},
+            no_logic_o = Object.keys(default_o)
+                .filter(k => k !== 'logic')
+                .reduce(_flat_obj.bind(null, default_o), {}),
+            flat_o = Object.keys(logic)
+                .reduce(_flat_obj.bind(null, logic), no_logic_o);
+
+        return Object.assign(o, {
+            [l_key]: flat_o
+        });
+    }, {});
+}
+
+function _flat_obj(o_tobe_flat, o_dest, k) {
+    return Object.assign(o_dest, { [k]: o_tobe_flat[k] });
+}
 
 export {
     lighting
--- src/lighting/blinn_phong/index.js
+++ src/lighting/blinn_phong/index.js
@@ -3,14 +3,10 @@ import * as config from './config.json';
 import * as vert from './shaders/vert.glsl';
 import * as frag from './shaders/frag.glsl';
 
-//"logic" API functions are exposed in the default object
 export default Object.assign({
+    logic,
     config,
     shaders: {
         vert, frag
     }
-}, Object.keys(logic).reduce(
-    (lo, l_key) => Object.assign(lo, {
-        [l_key]: logic[l_key]
-    })
-));
+});
