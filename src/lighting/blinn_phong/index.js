48839f021c29b7863087dd1ba9655920b68d5b8a
DanieleCapuano <el.einad@gmail.com>
2023-03-02 07:33:03 +0100

    logic in default object

--- src/lighting/blinn_phong/index.js
+++ src/lighting/blinn_phong/index.js
@@ -3,10 +3,14 @@ import * as config from './config.json';
 import * as vert from './shaders/vert.glsl';
 import * as frag from './shaders/frag.glsl';
 
-export default {
-    logic,
+//"logic" API functions are exposed in the default object
+export default Object.assign({
     config,
     shaders: {
         vert, frag
     }
-};
+}, Object.keys(logic).reduce(
+    (lo, l_key) => Object.assign(lo, {
+        [l_key]: logic[l_key]
+    })
+));
