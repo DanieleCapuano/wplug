dd728bdeb8e11915a89a5e00f4f157132f7451bf
DanieleCapuano <el.einad@gmail.com>
2023-03-06 15:58:57 +0100

    aa frag fix

--- src/plugins/antialiasing/fs_derivatives/shaders/frag.glsl
+++ src/plugins/antialiasing/fs_derivatives/shaders/frag.glsl
@@ -6,14 +6,12 @@ float aastep(float threshold , float value) {
 }
 
 vec2 aastep2d(float threshold , vec2 value) {
-    vec2 afwidth = 0.7 * length(vec2(dFdx(value.x), dFdy(value.y)));
-    // GLSL ' s fwidth ( value ) is abs ( dFdx ( value ) ) + abs ( dFdy ( value ) )
+    vec2 afwidth = 0.7 * fwidth(value);
     return smoothstep(threshold - afwidth , threshold + afwidth , value);
 }
 
 //value in this case is typically a color
 vec3 aastep3d(float threshold , vec3 value) {
     vec3 afwidth = 0.7 * fwidth(value);
-    // GLSL ' s fwidth ( value ) is abs ( dFdx ( value ) ) + abs ( dFdy ( value ) )
     return smoothstep(threshold - afwidth , threshold + afwidth , value);
 }
